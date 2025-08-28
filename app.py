589aa40b339f7207e3fe5aae8e0a25c0ab13ae6f
as <megumiru175@gmail.com>
2025-08-28 13:29:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,46 @@
-from flask import Flask,render_template
+import streamlit as st
+import os
+import pandas as pd
+
+
+st.title('ebaydata')
+st.caption('これはebayのデータ一覧です')
+
+col1,col2 = st.columns(2)
+
+with col1:
+
+    st.subheader('実験')
+    st.text('これは実験ページです。')
+
+    with st.form(key='profile_form'):
+
+        name = st.text_input('名前')
+        address= st.text_input('住所')
+
+        age_category = st.selectbox(
+        '年齢層',('子ども(18歳未満)','大人(18歳以上)')
+    )
+        hobby = st.multiselect(
+        '趣味',
+        ('スポーツ','読書','映画','料理'
+        ))
+
+        submit_btn = st.form_submit_button('実行')
+        cancel_btn = st.form_submit_button('キャンセル')
+    print(f'submit_btn:{submit_btn}')
+    print(f'cancel_btn:{cancel_btn}')
+
+    if submit_btn:
+        st.text(f'ようこそ！{name}さん！{address}なんですね！！')
+        st.text(f'年齢層:{age_category}')
+        st.text(f'趣味: {",".join(hobby)}')
+
+with col2:
+
+    df =pd.read_csv('data.csv')
+    st.line_chart(df)
+    st.bar_chart(df['2021年'])from flask import Flask,render_template
 from  ebaysoldgazo import get_ebay_items
 
 
